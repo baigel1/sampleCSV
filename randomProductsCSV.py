cbfdeaca7a7deed9b8032afc1decf8c84cc32e88
Daniel Baigel <dbaigel@yext.com>
2023-08-17 11:20:40 -0400

    updated id column

--- randomProductsCSV.py
+++ randomProductsCSV.py
@@ -21,10 +21,10 @@ def generate_random_price():
 # Generate and populate the CSV file
 with open("dummy_data.csv", mode="w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["id", "name", "color", "size", "description", "price"])
+    writer.writerow(["ID", "name", "color", "size", "description", "price"])
 
     for i in range(1000):
-        id = i + 1
+        id = str(i + 1) + fake.word()
         name = fake.word().capitalize()
         color = generate_random_color()
         size = generate_random_size()
